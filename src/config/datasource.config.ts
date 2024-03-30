6fdbdcaf70cc796371aad3b8b3b2dac72412990b
Luiz Fernando Consalter Foncatti <luifon@alunos.utfpr.edu.br>
2024-03-30 10:16:14 -0300

    DEV-0000: Uncommented DB initiation

--- src/config/datasource.config.ts
+++ src/config/datasource.config.ts
@@ -14,12 +14,12 @@ const AppDataSource = new DataSource({
   synchronize: true,
 });
 
-// AppDataSource.initialize()
-//   .then(() => {
-//     console.log('Data Source has been initialized!');
-//   })
-//   .catch((err) => {
-//     console.error('Error during Data Source initialization', err);
-//   });
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization', err);
+  });
 
 export default AppDataSource;
