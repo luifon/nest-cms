abb5969a1774d3e68d050c61924ee504716a5aff
Luiz Fernando Consalter Foncatti <luifon@alunos.utfpr.edu.br>
2024-03-30 10:27:45 -0300

    DEV-0000: Moved entity folder to entities folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,27 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { User } from './user/user.entity';
-import AppDataSource from './config/datasource.config';
+import { User } from './entities/user/user.entity';
+import { UsersModule } from './entities/user/user.module';
+
+const database: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'myuser',
+  password: 'mypassword',
+  database: 'cms_db',
+  schema: 'cms_schema',
+  entities: [User],
+  synchronize: true,
+};
 
 @Module({
   imports: [
-    TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-        type: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: 'myuser',
-        password: 'mypassword',
-        database: 'cms_db',
-        schema: 'cms_schema',
-        entities: [User],
-        synchronize: true,
-        dataSource: AppDataSource,
-      }),
-    }),
+    TypeOrmModule.forRootAsync({ useFactory: () => database }),
     TypeOrmModule.forFeature([User]),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
